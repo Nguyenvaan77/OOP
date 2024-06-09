04a1d67a8f877fa3a83c1b5d512fa70813be033e
Nguyenvaan77 <anvan16022005@gmail.com>
2024-06-09 22:13:55 +0700

    Add project files.

---
+++ OOP/main.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+class shape {
+public:
+	virtual int getArea() = 0;
+protected:
+	int height;
+	int width;
+};
+
+class rectangle:public shape{
+
+public:
+	int getArea() {
+		return height* width;
+	}
+};
+
+class tron :public shape {
+
+public:
+	int getArea() {
+		return (height * width)/2;
+	}
+};
+
+int main() {
+	return 0;
+}
